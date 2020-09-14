faac924ab63f20ad997042fb6b4f9b1bbeff84ad
Kaliza Anna <annkaliza@gmail.com>
2020-09-14 17:48:36 +0000

    added assertEqual file

---
+++ assertEqual.js
@@ -0,0 +1,22 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+
+  if (typeof actual === "string") {
+
+    actual = `"${actual}"`;
+    
+  }
+
+  if (typeof expected === 'string') {
+    expected = `"${expected}"`;
+  }
+
+  if (actual === expected) {
+    console.log(`👍 Assertion passed: ${actual} == ${expected}`);
+  } else {
+    console.log(`👎 Assertion failed: ${actual} != ${expected}`);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
