ed7250479cbf53cff12b94d35793d3198c97ab11
Kaliza Anna <annkaliza@gmail.com>
2020-09-16 09:11:31 +0000

    add assertArraysEqual file

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+const eqArrays = function (arr1, arr2) {
+  let n1 = arr1.length;
+  let n2 = arr2.length;
+  if (n1 !== n2) {
+    return false;
+  } else {
+    for (let i = 0; i < n1; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(firstArray, secondArray) {
+  
+
+  if (eqArrays(firstArray, secondArray)) {
+    console.log(`👍 Assertion passed: [${firstArray}] == [${secondArray}]`);
+  } else {
+    console.log(`👎 Assertion failed: [${firstArray}] != [${secondArray}]`);
+  }
+
+};
